daebf064eb3c10217b5ed32cba5335d8578a278a
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-05 14:53:43 -0300

    Update __init__.py

--- fragua_sets/steps/__init__.py
+++ fragua_sets/steps/__init__.py
@@ -1,5 +1,13 @@
 """Step functions module."""
 
-from .step_config import StepBuilder, StepIndex
+from .step_config import STEP_INDEX
+from .extraction import EXTRACTION_STEPS
+from .loading import LOADING_STEPS
+from .utility import UTILITY_STEPS
 
-__all__ = ["StepBuilder", "StepIndex"]
+__all__ = [
+    "STEP_INDEX",
+    "EXTRACTION_STEPS",
+    "LOADING_STEPS",
+    "UTILITY_STEPS",
+]
